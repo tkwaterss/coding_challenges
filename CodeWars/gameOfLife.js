5ab07abb9ee9f31f4fd354aa01be6b8c8334f4a3
Tobin Waters <tkwaterss@gmail.com>
2023-09-29 10:21:31 -0600

    Final Solution

--- CodeWars/gameOfLife.js
+++ CodeWars/gameOfLife.js
@@ -1,5 +1,4 @@
-//TODO Need to add a perimeter check around existing cells
-
+//! Unable to find correct solution on my own
 const getGeneration = (cells, generations) => {
   if (cells.filter((cell) => cell.includes(1)).length === 0) return [[]];
   
@@ -52,7 +51,27 @@ const getGeneration = (cells, generations) => {
     }
     cells = output.map((cell) => [...cell])
   }
-  //TODO Need to loop over matrix and remove extra 0's on exterior
+
+  //removing empty rows from top and bottom
+  while (!output[0].includes(1) || !output[output.length -1].includes(1)) {
+    if (!output[output.length - 1].includes(1)) output.pop()
+    if (!output[0].includes(1)) output.shift()
+  }
+
+  //removing empty columns from front and back
+  let emptyColumns = true;
+  do {
+    for (let cell of output) {
+      if (cell[0] === 1) emptyColumns = false;
+      if (cell[cell.length - 1] === 1) emptyColumns = false;
+    }
+    if (emptyColumns) {
+      for (let cell of output) {
+        cell.shift()
+        cell.pop()
+      }
+    }
+  } while (emptyColumns)
 
   return output;
 };
@@ -77,3 +96,61 @@ console.log(
     2
   )
 );
+
+//! Another users solution
+function getGeneration(cells, generations){
+
+  var ce = JSON.parse(JSON.stringify(cells));
+
+  var minY;
+  var maxY;
+  var minX;
+  var maxX;
+
+  for (var i = 1; i <= generations; i++) {
+    expandUniverse();
+
+    ce = ce.map(function(r, row){return r.map(function(c, col){return setValue(row, col, -c);});});
+
+    getBoundaries();
+    shrinkUniverse();
+  }
+
+  function setValue(row, col, sum) {
+    for (var r = row - 1; r <= row + 1; r++)
+      for (var c = col - 1; c <= col + 1; c++)
+        sum += (ce[r] && ce[r][c]) | 0;
+
+    return +(sum == 3 || sum == 2 && ce[row][col]);
+  }
+
+  function getBoundaries() {
+    minY = maxY = minX = maxX = -1;
+    
+    ce.forEach(function(r, row){r.forEach(function(c, col){
+      if (c == 1) {
+        minY = minY == -1 ? row : Math.min(minY, row);
+        maxY = maxY == -1 ? row : Math.max(maxY, row);
+        minX = minX == -1 ? col : Math.min(minX, col);
+        maxX = maxX == -1 ? col : Math.max(maxX, col);
+      }
+    });});
+  }
+
+  function expandUniverse() {
+    var a = [], b = [];
+
+    ce[0].forEach(function(){a.push(0); b.push(0);});
+
+    ce.unshift(a);
+    ce.push(b);
+    ce.map(function(i){i.unshift(0); i.push(0);});
+  }
+
+  function shrinkUniverse() {
+    ce = ce.map(function(e){ return e.slice(minX, maxX + 1);});
+    ce = ce.slice(minY, maxY + 1);
+  }
+
+  return ce;
+}
