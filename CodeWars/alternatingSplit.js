b0fb4663d11f19b4e241fff59580ed5ad0e39123
Tobin Waters <tkwaterss@gmail.com>
2023-10-23 11:42:22 -0600

    nearly completed alternating split encryption prolem

---
+++ CodeWars/alternatingSplit.js
@@ -0,0 +1,42 @@
+const encrypt = (text, n) => {
+  if (!text || n < 0) return text;
+  text = text.split('')
+  for (let i = 0; i < n; i++) {
+    let evens = []
+    let odds = []
+    for (let j = 0; j < text.length; j++) {
+      j % 2 === 0 ? evens.push(text[j]) : odds.push(text[j])
+    }
+    text = [...odds, ...evens]
+  }
+  return text.join('');
+}
+
+
+const decrypt = (encryptedText, n) => {
+  if (!encryptedText || n < 0) return encryptedText;
+  let text = encryptedText.split('')
+  let length = Math.floor(encryptedText.length / 2)
+  for (let i = 0; i < n; i++) {
+    let odds = text.slice(0, length);
+    let evens = text.slice(length);
+    let newText = []
+    while (odds.length > 0 || evens.length > 0) {
+      if (evens.length > 0) {
+        newText.push(evens[0])
+        evens.shift()
+      }
+      if (odds.length > 0) {
+        newText.push(odds[0])
+        odds.shift()
+      }
+    }
+    text = newText;
+  }
+  return text.join('');
+}
+console.log(encrypt('012345', 2))
+console.log(encrypt('0123456', 1))
+
+console.log(decrypt('135024', 2))
+console.log(decrypt('1350246', 1))
