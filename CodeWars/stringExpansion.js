b06bdd0a3a4a21611c19e319caf0ac4d2443f68e
Tobin Waters <tkwaterss@gmail.com>
2023-10-30 10:17:11 -0600

    completed string expansion problem

--- CodeWars/stringExpansion.js
+++ CodeWars/stringExpansion.js
@@ -1,28 +1,21 @@
-const numbers = '0123456789'.split('');
-const lowerCase = 'abcdefghijklmnopqrstuvwxyz'.split('');
-const upperCase = lowerCase.join('').toUpperCase().split('');
+
 const stringExpansion = string => {
-  if (!string || !string.includes(...lowerCase, ...upperCase)) return ''
-  string = string.split('');
+  const numbers = '0123456789'.split('');
+
   let expanded = []
-  for (let i = 0; i < string.length - 1; i++) {
-    if (!string[i + 1]) {
-      
-    }
+  let multiplier = 1;
 
-    if (numbers.includes(string[i]) && !numbers.includes(string[i + 1])) {
-      expanded.push(string[i + 1].repeat(+string[i]))
-    }
-    
-    
-    if (!numbers.includes(string[i])) expanded.push(string[i])
+  for (let i = 0; i < string.length; i++) {
+    if (numbers.includes(string[i])) multiplier = +string[i]
 
+    if (!numbers.includes(string[i])) {
+      expanded.push(string[i].repeat(multiplier)) 
+    }
   }
-
-  return expanded
+  return expanded.join('')
 }
 
 console.log(stringExpansion(''))
 console.log(stringExpansion('3abc'))
-console.log(stringExpansion('a111'))
+console.log(stringExpansion('aaaaa'))
 console.log(stringExpansion('3d332f2a'))
