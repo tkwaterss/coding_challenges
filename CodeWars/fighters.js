b163bca687557ab6f4f4c56febd9566b374e38dd
Tobin Waters <tkwaterss@gmail.com>
2023-11-01 09:16:28 -0600

    completed fighter problem

--- CodeWars/fighters.js
+++ CodeWars/fighters.js
@@ -8,9 +8,27 @@ const declareWinner = (fighter1, fighter2, firstAttack) => {
     first = fighter2;
     second = fighter1;
   }
-  while (first.health >= 0 || second.health >= 0) {
+  while (first.health > 0 && second.health > 0) {
     second.health -= first.damagePerAttack;
-    first.health -= second.damagePerAttack;
+    if (second.health >= 0) {
+      first.health -= second.damagePerAttack;
+    }
   }
-  return first.health <= 0 ? first.name : second.name;
+  
+  let winner = first.health <= 0 ? second : first;
+  console.log(winner)
+  return winner.name
 }
+const jerry = {
+  name: 'jerry',
+  health: 30,
+  damagePerAttack: 3,
+}
+const bob = {
+  name: 'bob',
+  health: 30,
+  damagePerAttack: 3,
+}
+
+
+console.log(declareWinner(jerry, bob, "Jerry"))
